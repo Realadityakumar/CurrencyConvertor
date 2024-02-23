d517da93e6c44910d5d047c455af764cf8396dc9
realadityakumar <adityakumar23214@lpu.in>
2024-02-23 22:43:36 +0530

    Add currency API URL and dropdown functionality

---
+++ app.js
@@ -0,0 +1,6 @@
+const base_url ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const dropdowns = document.querySelectorAll('.dropdown');
+
+for (code in countryList){
+    console.log(code, countryList[code]);
+}
